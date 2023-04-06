aa87b60fb7dee0c2b8f1f69f0821568eded58840
prathamrehil <prathamrehil@gmail.com>
2023-04-06 08:45:21 +0530

    commit4

--- lib/sql_helper.dart
+++ lib/sql_helper.dart
@@ -17,7 +17,7 @@ class SQLHelper {
 
   static Future<sql.Database> db() async {
     return sql.openDatabase(
-      'kindacode.db',
+      'inventorymanager.db',
       version: 1,
       onCreate: (sql.Database database, int version) async {
         await createTables(database);
